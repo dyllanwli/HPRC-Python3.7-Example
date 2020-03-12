1a9dd5f5ad87651ce08c85422c7816bb1ac6a12b
dyllanwli <messsking@gmail.com>
2020-03-11 23:17:58 -0500

    add nohup exec

---
+++ nohup-exec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This scripts is a happy path for running python program on HPRC using nohup
+
+module load Python/3.7.0-intel-2018b
+
+virtualenv venv
+
+source venv/bin/activate
+
+nohup python test.py & >> nohup.out 2$1
+
+######### Deactivate
+# deactivate 
